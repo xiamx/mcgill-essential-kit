54fee89a9eb6fde6e6b9f89043ba307bab854575
Mengxuan Xia <xiamx2004@gmail.com>
2012-10-05 09:59:01 -0400

    Added the initial version of elegantMycourses2.user.js elegantMycourses2 is a greasemonkey/userscript that boost the user interface of mycourses2.mcgill.ca

---
+++ myCourses2/elegantMycourses2.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @match https://mycourses2.mcgill.ca/d2l/lms/discussions/messageLists/frame_right*
+// ==/UserScript==
+setTimeout(function(){
+	var frameright = window;
+	var frameset = frameright.document.getElementsByTagName("frameset")[0]; 
+	frameset.setAttribute("rows","99%,1%");
+	frameset.setAttribute("cols","50%, 50%");
+	
+	frameset.removeAttribute("rows");
+},10);
+
